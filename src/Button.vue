d7e688bb665afab062948cb25147520a6166ab1c
xiao <779740337@qq.com>
2021-04-05 16:41:33 +0800

    验证提交的代码是否符合要求

--- src/Button.vue
+++ src/Button.vue
@@ -1,29 +1,68 @@
 <template>
-    <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg class="icon" v-if="icons">
+      <use :xlink:href="`#i-${icons}`"></use>
+    </svg>
+    <slot></slot>
+  </button>
 </template>
-
 <script>
 export default {
   name: "Button",
+  // props:["icons","iconPosition"],
+  props:{
+    icons: {},
+    iconPosition:{
+      type:String,
+      default:"left",
+      validator(value){
+        return !(value !== "left" && value !== "right")
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.g-button{
+.g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   font-size: var(--font-size);
   height: var(--button-height);
+  vertical-align: middle;
   padding: 0 1em;
   border-radius: var(--button-radius);
-  border:1px solid var(--border-color);
+  border: 1px solid var(--border-color);
   background: var(--button-bg);
-}
-.g-button:hover{
-  border-color: var(--border-color-hover);
-}
-.g-button:active{
-  background-color: var(--button-active-background);
-}
-.g-button:focus{
-  outline: none;
+  .icon{
+    width: 1em;
+    height: 1em;
+    display: none;
+  }
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-background);
+  }
+
+  &:focus {
+    outline: none;
+  }
+  &.icon-right{
+    >.icon{
+      display: block;
+      order: 2;
+      margin-left: 0.2em;
+    }
+  }
+  &.icon-left{
+    >.icon{
+      display: block;
+      margin-right: 0.2em;
+    }
+  }
 }
 </style>
